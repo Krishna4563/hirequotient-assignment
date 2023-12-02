d048741af47590f09fbf31ea58afa2944288ecf2
Krishna4563 <daskrishna0902@gmail.com>
2023-12-02 22:07:03 +0530

    content added

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,24 @@ import React from "react";
 import "../components/home.css";
 
 const home = () => {
-  return <section className="section-1">Hi</section>;
+  return (
+    <section className="section-1">
+      <div className="text1">WELCOME TO MANAGE WISE!</div>
+      <div className="text2">
+        Empower your buisness with{" "}
+        <span className="hlght-text">Strategic </span>
+        insights
+      </div>
+      <div className="text3">
+        Powerful management platform designed to streamline your business
+        operations, boost productivity, and drive success
+      </div>
+      <div className="text4">
+        <button className="started-btn">Get Started</button>
+        <button className="demo-btn">Watch Demo</button>
+      </div>
+    </section>
+  );
 };
 
 export default home;
